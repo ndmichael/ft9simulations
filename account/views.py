3b9b8ff28965e0b7e19636fa768e04346e2ab9b2
ndmichael <ukejemicheal@gmail.com>
2023-10-14 10:40:46 +0100

    redirecting users

--- account/views.py
+++ account/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
+from django.urls import reverse
 
-# Create your views here.
+
+
+class CustomLoginView(LoginView):
+
+    def get_success_url(self):
+        url = self.get_redirect_url()
+        return url or reverse('staff-detail', kwargs={'id': self.request.user.pk, 'username': self.request.user.username})
