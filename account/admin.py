ad4347974b244a2dbdf66d126a491bef65fb47a6
ndmichael <ukejemicheal@gmail.com>
2023-10-17 00:35:30 +0100

    customized admin for abstractuser

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,41 @@
 from django.contrib import admin
+from account.models import CustomUser
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 # Register your models here.
+
+# admin.site.register(CustomUser)
+
+@admin.register(CustomUser)
+class UserAdmin(BaseUserAdmin):
+    list_filter = ["is_staff"]
+    fieldsets = [
+        (None, {"fields": ["username", "email", "password", "is_staff", "is_active"]}),
+        (
+            "Other info",
+            {
+                "fields": [
+                    "first_name",
+                    "last_name",
+                ],
+            },
+        ),
+    ]
+    add_fieldsets = [
+        (
+            None,
+            {
+                "fields": [
+                    "username",
+                    "email",
+                    "first_name",
+                    "last_name",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active"
+                ]
+            },
+        ),
+    ]
+
